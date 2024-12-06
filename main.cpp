089362275431769c4fbd033be67a823209f63a2e
Folco <martial.demolins@gmail.com>
2024-12-06 17:47:33 +0100

    - reformatted GPL notices - added window and application icons
    Signed-off-by: Folco <martial.demolins@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,33 +1,38 @@
-/*
- * TBI - Technical Bulletin Indexer - Save and index Technical Bulletins,
- * allowing to use keywords to find them easily
- * Copyright (C) 2020 Martial Demolins AKA Folco
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * mail: martial <dot> demolins <at> gmail <dot> com
- */
+/*******************************************************************************
+ *                                                                             *
+ *   TBI - Technical Bulletin Indexer - Save and index Technical Bulletins.    *
+ *        Provide a search engine and documentations download features.        *
+ *             Copyright (C) 2020-2025 Martial Demolins AKA Folco              *
+ *                                                                             *
+ *    This program is free software: you can redistribute it and/or modify     *
+ *    it under the terms of the GNU General Public License as published by     *
+ *      the Free Software Foundation, either version 3 of the License, or      *
+ *                      at your option) any later version                      *
+ *                                                                             *
+ *       This program is distributed in the hope that it will be useful        *
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *                 GNU General Public License for more details                 *
+ *                                                                             *
+ *      You should have received a copy of the GNU General Public License      *
+ *     along with this program. If not, see <https://www.gnu.org/licenses      *
+ *                                                                             *
+ *              mail: martial <dot> demolins <at> gmail <dot> com              *
+ *                                                                             *
+ ******************************************************************************/
 
 #include "Global.hpp"
 #include "UI/MainWindow.hpp"
 #include <QApplication>
+#include <QGuiApplication>
+#include <QIcon>
 
 int main(int argc, char* argv[])
 {
     QApplication Application(argc, argv);
-    bool         ForceIndexCheck = Application.arguments().contains(OPTION_FORCE_INDEX_CHECK);
-    MainWindow   Window(ForceIndexCheck);
+    QGuiApplication::setWindowIcon(QIcon(":/Main/Icon.png"));
+    bool       ForceIndexCheck = Application.arguments().contains(OPTION_FORCE_INDEX_CHECK);
+    MainWindow Window(ForceIndexCheck);
     Window.show();
     return Application.exec();
 }
